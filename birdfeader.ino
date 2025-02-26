5ba91ed0a981b5e416b9fa0ebcd065803ae4279e
M0D <167003676+M0D42@users.noreply.github.com>
2025-02-26 10:14:18 -0600

    Update birdfeader.ino

--- birdfeader.ino
+++ birdfeader.ino
@@ -31,76 +31,15 @@
 camera_config_t config;
 //this are the parameter of the camera this allow you to change some seting like chaning jpeg
 void camaras_parameter(){
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sccb_sda = SIOD_GPIO_NUM;
-    config.pin_sccb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_JPEG; 
 }
 
 void sdcardinit(){
-//Serial.println("Starting SD Card");
-if(!SD_MMC.begin()){
-    Serial.println("SD Card Mount Failed");
-    return;
-  }
-   
-  uint8_t cardType = SD_MMC.cardType();
-  if(cardType == CARD_NONE){
-    Serial.println("No SD Card attached");
-    return;
-  }
 }
 
 void saveImage(){
-    EEPROM.begin(EEPROM_SIZE);
-    pictureNumber = EEPROM.read(0) + 1;
-  
-    // Path where new picture will be saved in SD Card
-    String path = "/picture" + String(pictureNumber) +".jpg";
-  
-    fs::FS &fs = SD_MMC; 
-    Serial.printf("Picture file name: %s\n", path.c_str());
-    
-    File file = fs.open(path.c_str(), FILE_WRITE);
-    if(!file){
-      Serial.println("Failed to open file in writing mode");
-    } 
-    else {
-      file.write(fb->buf, fb->len); // payload (image), payload length
-      Serial.printf("Saved file to path: %s\n", path.c_str());
-      EEPROM.write(0, pictureNumber);
-      EEPROM.commit();
-    }
-    file.close();
-    esp_camera_fb_return(fb); 
-    
 }
 
 void takepicture(){
-        camera_fb_t * fb = NULL;
-    // Take Picture with Camera
-    fb = esp_camera_fb_get();  
-    if(!fb) {
-    Serial.println("Camera capture failed");
-    return;
-    }
-    saveImage();
 }
 void setup(){
     WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
@@ -127,7 +66,8 @@ sdcardinit();
 }
 
 void loop(){
-
+takepicture();
+delay(5000);
 
 
 }
