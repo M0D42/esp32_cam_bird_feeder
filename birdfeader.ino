34c7d7b19d59017e30e418fc5caa54918d55a6af
M0D <167003676+M0D42@users.noreply.github.com>
2025-03-03 04:35:43 +0000

    Update birdfeader.ino

--- birdfeader.ino
+++ birdfeader.ino
@@ -1,25 +1,23 @@
+//camera libaries
 #include "esp_camera.h"
-#include "Arduino.h"
-#include "FS.h"                // SD Card ESP32
-#include "SD_MMC.h"            // SD Card ESP32
-#include "soc/soc.h"           // Disable brownour problems
-#include "soc/rtc_cntl_reg.h"  // Disable brownour problems
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 #include "driver/rtc_io.h"
-#include <EEPROM.h>            // read and write from flash memory
-#include "driver/rtc_io.h"
-// define the number of bytes you want to access
-#define EEPROM_SIZE 1
-//deepsleep button
+
+// SD Libraries
+#include "FS.h"
+#include "SD_MMC.h"
+//pins for the pir sensor to be able to wake up
+const int wakeupPin = 13;  // GPIO 4 for external wake-up
+RTC_DATA_ATTR int bootCount = 0;  // Number of reboots
 
 
-
-// Pin definition for CAMERA_MODEL_AI_THINKER
+// Pin definitions for CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
 #define SIOD_GPIO_NUM     26
 #define SIOC_GPIO_NUM     27
-
 #define Y9_GPIO_NUM       35
 #define Y8_GPIO_NUM       34
 #define Y7_GPIO_NUM       39
@@ -31,108 +29,192 @@
 #define VSYNC_GPIO_NUM    25
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
-//this are the parameter of the camera this allow you to change some seting like chaning jpeg
-int pictureNumber = 0;
-camera_config_t config;
-void camaras_parameter(){
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sccb_sda = SIOD_GPIO_NUM;
-    config.pin_sccb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_JPEG; 
-}
 
-void sdcardinit(){
-//Serial.println("Starting SD Card");
-if(!SD_MMC.begin()){
-    Serial.println("SD Card Mount Failed");
+void parameters() {
+  // Configure Camera parameters
+
+  // Object to store the camera configuration parameters
+  camera_config_t config;
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; // Choices are YUV422, GRAYSCALE, RGB565, JPEG
+
+  // Select lower framesize if the camera doesn't support PSRAM
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10; //10-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Initialize the Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
 
+  // Camera quality adjustments
+  sensor_t * s = esp_camera_sensor_get();
+
+// BRIGHTNESS (-2 to 2)
+s->set_brightness(s, 1);  // Boost brightness
+// CONTRAST (-2 to 2)
+s->set_contrast(s, 1);    // Add a bit of contrast to sharpen details
+  // SATURATION (-2 to 2)
+  s->set_saturation(s, 1);
+  // SPECIAL EFFECTS (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+  s->set_special_effect(s, 0);
+  // WHITE BALANCE (0 = Disable , 1 = Enable)
+// WHITE BALANCE (0 = Disable, 1 = Enable)
+s->set_whitebal(s, 1);  // Ensure white balance is on
+// AWB GAIN (0 = Disable, 1 = Enable)
+s->set_awb_gain(s, 1);  // Enable auto white balance gain
+// WB MODES (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+s->set_wb_mode(s, 2);   // Try "Cloudy" mode for more neutral colors
+// AGC GAIN (0 to 30)
+s->set_agc_gain(s, 5);  // Increase gain slightly to balance color intensity
+
+  // EXPOSURE CONTROLS (0 = Disable , 1 = Enable)
+  s->set_exposure_ctrl(s, 1);
+  // AEC2 (0 = Disable , 1 = Enable)
+  s->set_aec2(s, 0);
+  // AE LEVELS (-2 to 2)
+  s->set_ae_level(s, 0);
+  // AEC VALUES (0 to 1200)
+  s->set_aec_value(s, 300);
+  // GAIN CONTROLS (0 = Disable , 1 = Enable)
+  s->set_gain_ctrl(s, 1);
+
+  // GAIN CEILING (0 to 6)
+  s->set_gainceiling(s, (gainceiling_t)0);
+  // BPC (0 = Disable , 1 = Enable)
+  s->set_bpc(s, 0);
+  // WPC (0 = Disable , 1 = Enable)
+  s->set_wpc(s, 1);
+  // RAW GMA (0 = Disable , 1 = Enable)
+  s->set_raw_gma(s, 1);
+  // LENC (0 = Disable , 1 = Enable)
+  s->set_lenc(s, 1);
+  // HORIZ MIRROR (0 = Disable , 1 = Enable)
+  s->set_hmirror(s, 0);
+  // VERT FLIP (0 = Disable , 1 = Enable)
+  s->set_vflip(s, 0);
+  // DCW (0 = Disable , 1 = Enable)
+  s->set_dcw(s, 1);
+  // COLOR BAR PATTERN (0 = Disable , 1 = Enable)
+  s->set_colorbar(s, 0);
+
+}
+
+void initMicroSDCard() {
+  // Start the MicroSD card
+
+  Serial.println("Mounting MicroSD Card");
+  if (!SD_MMC.begin()) {
+    Serial.println("MicroSD Card Mount Failed");
+    return;
+  }
   uint8_t cardType = SD_MMC.cardType();
-  if(cardType == CARD_NONE){
-    Serial.println("No SD Card attached");
+  if (cardType == CARD_NONE) {
+    Serial.println("No MicroSD Card found");
     return;
   }
-}
 
-void saveImage(camera_fb_t * fb){
-    EEPROM.begin(EEPROM_SIZE);
-    pictureNumber = EEPROM.read(0) + 1;
-
-    // Path where new picture will be saved in SD Card
-    String path = "/picture" + String(pictureNumber) +".jpg";
-
-    fs::FS &fs = SD_MMC; 
-    Serial.printf("Picture file name: %s\n", path.c_str());
-
-    File file = fs.open(path.c_str(), FILE_WRITE);
-    if(!file){
-      Serial.println("Failed to open file in writing mode");
-    } else {
-      file.write(fb->buf, fb->len); // payload (image), payload length
-      Serial.printf("Saved file to path: %s\n", path.c_str());
-      EEPROM.write(0, pictureNumber);
-      EEPROM.commit();
-    }
-    file.close();
-    esp_camera_fb_return(fb); 
-
-    // Turns off the ESP32-CAM white on-board LED (flash) connected to GPIO 4
- }
-
-void takepicture(){
-    camera_fb_t * fb = NULL;  
-    // Take Picture with Camera
-    fb = esp_camera_fb_get();  
-    if(!fb) {
-        Serial.println("Camera capture failed");
-        return;
-    }
-    saveImage(fb);
 }
 
 
+void takeNewPhoto(String path) {
+  // Take Picture with Camera
 
-void setup(){
-    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
-    Serial.begin(115200);
-    camaras_parameter;
-    pinMode(pir, INPUT);
-    //if your esp32 has more ram you can use this to take better pictures
-    if(psramFound()){
-        config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-        config.jpeg_quality = 10;
-        config.fb_count = 2;
-      }else {
-        config.frame_size = FRAMESIZE_SVGA;
-        config.jpeg_quality = 12;
-        config.fb_count = 1;
+  // Setup frame buffer
+  camera_fb_t  * fb = esp_camera_fb_get();
+
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+
+  // Save picture to microSD card
+  fs::FS &fs = SD_MMC;
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file in write mode");
+  }
+  else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.printf("Saved file to path: %s\n", path.c_str());
+  }
+  // Close the file
+  file.close();
+
+  // Return the frame buffer back to the driver for reuse
+  esp_camera_fb_return(fb);
 }
-// initiates camera
-esp_err_t err = esp_camera_init(&config);
-if (err != ESP_OK) {
-  Serial.printf("Camera init failed with error 0x%x", err);
-  return;
+
+void pir(){
+
+
+
 }
-sdcardinit();
+void setup() {
+  // Disable brownout detector
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+  // Start Serial Monitor
+  Serial.begin(115200);
+  //boot and pir stuff
+  pinMode(wakeupPin, INPUT);  // Declaring the pin with the push button as INPUT_PULLUP
+  bootCount++;  // Increment the number of boots by 1
+  Serial.println("Boot number: " + String(bootCount));  // Print the boot number
+  // Initialize the camera
+  parameters();
+  Serial.println("Camera OK!");
+
+  // Initialize the MicroSD
+  initMicroSDCard();
+
+  // initialize EEPROM with predefined size
+
+
+  // Path where new picture will be saved in SD Card
+  String path = "/image" + String(bootCount) + ".jpg";
+  Serial.printf("Picture file name: %s\n", path.c_str());
+
+  // Take and Save Photo
+  takeNewPhoto(path);
+
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)wakeupPin, HIGH);  // Configure external wake-up
+  Serial.println("Entering sleep mode");
+  delay(1000);
+
+  // Enter deep sleep mode
+  esp_deep_sleep_start();
+
+
 }
-void loop(){
-takepicture();
-delay(5000); // Take a picture every 5 seconds
+
+void loop() {
+//the setup is the loop
 }
